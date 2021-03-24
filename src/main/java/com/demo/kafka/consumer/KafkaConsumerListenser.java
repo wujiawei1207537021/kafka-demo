5d98cbfc4f92d800549b0134e27cad29d79ce909
wujiawei <1207537021@qq.com>
2021-03-24 11:16:24 +0800

    添加多线程方式demo 消费kafka 数据

--- src/main/java/com/demo/kafka/consumer/KafkaConsumerListenser.java
+++ src/main/java/com/demo/kafka/consumer/KafkaConsumerListenser.java
@@ -1,9 +1,11 @@
 package com.demo.kafka.consumer;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -26,6 +28,12 @@ public class KafkaConsumerListenser {
         List<Map<String, String>> dataList = new ArrayList<Map<String, String>>();
         String tabliName = null;
         System.out.println(records);
+    }
+
+
+    @KafkaListener(groupId = "wujiawei", concurrency = "100", topics = {"kafka_test"})
+    public void processMessage(ConsumerRecord<String, String> record) throws IOException {
+        System.out.println(record.value());
     }
 }
 
